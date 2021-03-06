907e8a7e116cf76034353500da2d9385f6285262
Bars <barsbekclash@gmail.com>
2021-03-06 12:20:38 +0600

    add models and migrate and register admin form

--- exam_1_django/webapp/admin.py
+++ exam_1_django/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Hotel
 
 # Register your models here.
+
+class ModernAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'time_start', 'status']
+    list_filter = ['status']
+    search_fields = ['name', 'status']
+
+
+admin.site.register(Hotel, ModernAdmin)
--- exam_1_django/webapp/models.py
+++ exam_1_django/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    mail = models.EmailField(max_length=254, null=False, blank=False,)
+    text = models.TextField(max_length=3000, null=False, blank=False)
+    time_start = models.DateTimeField(auto_now_add=True, null=False, blank=True)
+    time_edit = models.DateTimeField(auto_now=True, blank=True, null=False)
+    status = models.CharField(max_length=200, null=False, blank=False, default='active')
+
+
+    class Meta:
+        db_table = 'Hotel'
+        verbose_name = 'Регистрация'
+        verbose_name_plural = 'Регистрации'
+
+    def __str__(self):
+        return f'{self.id}. {self.time_start}: {self.name} - {self.status} - {self.time_edit}'
